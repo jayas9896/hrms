ffd6ee304ee1156e2c1e60ed4778d2b36290a263
Jayashanker Padishala <jayashankar.pjs@gmail.com>
2026-05-09 16:37:33 -0700

    fix(frontend): support symlinked bench builds

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,4 @@
 import { io } from "socket.io-client"
-import { socketio_port } from "../../../../sites/common_site_config.json"
 
 import { getCachedListResource } from "frappe-ui/src/resources/listResource"
 import { getCachedResource } from "frappe-ui/src/resources/resources"
@@ -7,7 +6,9 @@ import { getCachedResource } from "frappe-ui/src/resources/resources"
 export function initSocket() {
 	let host = window.location.hostname
 	let siteName = window.site_name
-	let port = window.location.port ? `:${socketio_port}` : ""
+	let socketioPort =
+		window.frappe?.boot?.socketio_port || window.socketio_port || ""
+	let port = window.location.port && socketioPort ? `:${socketioPort}` : ""
 	let protocol = port ? "http" : "https"
 	let url = `${protocol}://${host}${port}/${siteName}`
 	let socket = io(url, {
